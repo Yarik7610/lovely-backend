2345ab1d7866c2eb042ff89f6ee78ca51c282b02
Yaroslav Yanovich <arikarik782@gmail.com>
2025-03-26 20:00:36 +0300

    style: update prettier config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './core/auth/auth.module';
+import { Module } from "@nestjs/common"
+import { AuthModule } from "./core/auth/auth.module"
 
 @Module({
   imports: [AuthModule],
   controllers: [],
-  providers: [],
+  providers: []
 })
 export class AppModule {}
