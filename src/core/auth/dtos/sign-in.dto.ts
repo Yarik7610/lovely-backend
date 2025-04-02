33803fd817869003581283f7879a4e5a8c24baa1
Yaroslav Yanovich <arikarik782@gmail.com>
2025-04-02 17:30:48 +0300

    chore: update default app port, update auth dtos

--- src/core/auth/dtos/sign-in.dto.ts
+++ src/core/auth/dtos/sign-in.dto.ts
@@ -1,7 +1,8 @@
-import { IsEmail } from "class-validator"
+import { IsEmail, MaxLength } from "class-validator"
 
 export class SignInDto {
   @IsEmail()
+  @MaxLength(50)
   email: string
 
   password: string
