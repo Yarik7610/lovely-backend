fd15989cf5c35a5c1b1ade06e5824bff6911dadb
Yaroslav Yanovich <arikarik782@gmail.com>
2025-04-14 21:16:15 +0300

    feat: add cors for all origins

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.setGlobalPrefix("api")
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
   app.use(cookieParser())
 
